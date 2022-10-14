38e110de999b54dc6521cf3155efd745117d81ce
PavanHathwar <pavan.hathwar@thoughtworks.com>
2022-10-14 16:08:39 +0530

    [pavan] initiall commit with database setup

---
+++ src/main/java/com/example/hotelbackend/modal/HotelEmployeeDetails.java
@@ -0,0 +1,34 @@
+package com.example.hotelbackend.modal;
+
+import javax.persistence.Entity;
+
+@Entity
+public class HotelEmployeeDetails {
+    private long id;
+    private String name;
+    private String emailId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
